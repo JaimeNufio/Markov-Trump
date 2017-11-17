da3dc13ef0a0b1edcf5ee3f9b2546519baa2b22e
Jaime Nufio <jaimeenufio@gmail.com>
2017-11-17 12:48:12 -0500

    Deleted Config

--- config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-        consumer_key:   'YtvbpQP7XPBVKILq4fvO7Rwqu',
-        consumer_secret: 'jZcLoHfX6YAWWtnul0NRnKXx35bG5Yo31Q04q9JKAd3Y5jJ1NS',
-        access_token: '847210224176041984-rebCplRChOetMu3PLwS0SS1zN9tawJR',
-        access_token_secret: 'KX9fpEQOLXvAeP3hvFiyoto0FqKaokBbAOIE0EBTLU0FP'
-}
-
